821dafe938e2699960d0551d5dc3f6d3a5dba3fb
Jarod Sneed <jarodsneed@Jarods-MacBook.local>
2019-06-29 07:40:16 -0400

    commit before db seeding

--- db/migrate/20190629112432_drop_game_titles_table.rb
+++ db/migrate/20190629112432_drop_game_titles_table.rb
@@ -1,6 +1,6 @@
 class DropGameTitlesTable < ActiveRecord::Migration[5.2]
   def change
-    drop_table :merchants do |t|
+    drop_table :game_titles do |t|
       t.string :name, null: false
     end
   end
